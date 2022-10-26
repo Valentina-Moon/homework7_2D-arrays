1ac7771126314faa3266d0d80e1161934c42dffc
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-27 02:55:40 +0300

    меняем строки

---
+++ Task№7/Program.cs
@@ -0,0 +1,44 @@
+﻿//Написать программу, которая обменивает элементы первой строки и последней строки.
+
+void FillArray(int[,] matr) 
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] array) 
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void Change(int[,] array)
+{ 
+    int temp = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        temp = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = temp;
+    }
+}
+Console.WriteLine("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[,] array = new int[m, n];
+Console.WriteLine("Изначальный массив: ");
+FillArray(array);
+PrintArray(array);
+Console.WriteLine("Массив в котором поменяли элементы первой и последней строки: ");
+Change(array);
+PrintArray(array);
