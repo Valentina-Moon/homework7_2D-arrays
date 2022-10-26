dc1642b79884ac647426ea83aa07ca7f6d8fb912
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-27 01:23:37 +0300

    позиции числа, заданного пользователем или указать, что такого элемента нет/

--- Task№4/Program.cs
+++ Task№4/Program.cs
@@ -9,7 +9,7 @@ void PrintArray(int[,] matr, int m, int n)
         {
             Console.Write($"{matr[i, j]} ");
         }
-    Console.WriteLine();
+        Console.WriteLine();
     }
 }
 void FillArray(int[,] matr)
@@ -18,31 +18,34 @@ void FillArray(int[,] matr)
         for (int j = 0; j < matr.GetLength(1); j++)
             matr[i, j] = new Random().Next(0, 10);
 }
-void FindIndex( int[,] matr, int number)
+
+void FindIndex(int[,] matr, int number)
 {
     for (int i = 0; i < matr.GetLength(0); i++)
         for (int j = 0; j < matr.GetLength(1); j++)
-            if (matr[i,j] == number) Console.Write($"Данное число находиться на позиции: {i}; {j}; ");
+            if (matr[i, j] == number) Console.Write($"Данное число находиться на позиции: {i}; {j}; ");
 }
-
 bool FindNumber(int[,] matr, int number)
 {
     for (int i = 0; i < matr.GetLength(0); i++)
+
         for (int j = 0; j < matr.GetLength(1); j++)
-            if (matr[i,j] == number) return true;
+
+            if (matr[i, j] == number) return true;
     return false;
 }
+
 
 Console.WriteLine("Количество строк: ");
 int m = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine("Количество столбцов: ");
 int n = int.Parse(Console.ReadLine() ?? "0");
 
-int [,] matrix = new int[n, m];
+int[,] matrix = new int[n, m];
 Console.WriteLine("Массив: ");
 FillArray(matrix);
 PrintArray(matrix, m, n);
 Console.WriteLine("Введите число");
-int num = int.Parse(Console.ReadLine()?? "0");
-if(FindNumber(matrix, num)) FindIndex(matrix, num);
+int num = int.Parse(Console.ReadLine() ?? "0");
+if (FindNumber(matrix, num)) FindIndex(matrix, num);
 else Console.WriteLine("Такого элемента нет");
