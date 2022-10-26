65557a7e646edb77c3d374a7924888a9c5ebfe62
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-26 20:01:06 +0300

    Показать двумерный массив размером m×n заполненный вещественными числами.

---
+++ Task№1/Program.cs
@@ -0,0 +1,39 @@
+﻿//Показать двумерный массив размером m×n заполненный вещественными числами.
+
+void FillArray(double[,] matr, int m , int n)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().NextDouble() * 100;
+            Console.Write($"{matr[i, j]}\t");
+        }
+    }
+}
+
+void PrintArray(double[,] matr, int m, int n)
+{
+
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Количество строк: ");
+ int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Количество столбцов: ");
+ int n =  int.Parse(Console.ReadLine() ?? "0");
+
+
+double[,] matrix = new double [m, n];
+
+
+FillArray(matrix, m, n);
+PrintArray(matrix, m, n);
